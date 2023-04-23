1e17c3921c41326360aabb549ae236ad72affd6c
ehz0ah <130889443+ehz0ah@users.noreply.github.com>
2023-04-23 17:19:57 +0800

    Time: 29 ms (97.37%) | Memory: 13.8 MB (64.32%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,18 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        make a pointer dum and a empty node dummy
+        iterate through the two list
+        compare the value of the first nodes of the each list
+        then set pointer to the node with the lower value
+        then reset original empty node to be the node that u previously set your pointer to 
+        repeat
+        '''
         dum = dummy = ListNode()
-        while list1 and list2:               
+        while list1 and list2:
             if list1.val < list2.val:
                 dum.next = list1
-                list1, dum = list1.next, list1
+                list1 , dum = list1.next, list1
             else:
                 dum.next = list2
                 list2, dum = list2.next, list2
-                
-        if list1 or list2:
-            dum.next = list1 if list1 else list2
-            
+        if list1 == None or list2 == None:
+            dum.next = list1 if list1 else list2 
         return dummy.next
-        
+
         
