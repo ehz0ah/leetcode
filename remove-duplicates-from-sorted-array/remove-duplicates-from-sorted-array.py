6a2a44219cf459bd1ec6775a9bf628213d0bb6d7
ehz0ah <130889443+ehz0ah@users.noreply.github.com>
2023-04-23 18:41:26 +0800

    Time: 88 ms (63.79%) | Memory: 15.6 MB (48.42%) - LeetSync

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,10 +1,16 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        '''
-        nums =  doesn't replace elements in the original list.
-        nums[:] = replaces element in place
-        '''
-        nums[:] = sorted(set(nums))
-        return len(nums)
-        
-
+  def removeDuplicates(self, nums: List[int]) -> int:
+    '''
+    make a slow pointer that points at the 0th index and a fast pointer at the 1st index
+    the slow pointer will increment only when the slow and fast pointer points at different objects
+    the fast pointer will iterate through the array to look for new unique elemnets
+    '''
+    slow , fast = 0 , 1
+    while fast in range(len(nums)):
+      if nums[slow] == nums[fast]:
+        fast += 1
+      else:
+        nums[slow + 1] = nums[fast]
+        fast += 1
+        slow += 1
+    return slow + 1
