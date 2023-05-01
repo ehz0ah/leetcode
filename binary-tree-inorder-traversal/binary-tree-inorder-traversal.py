4858a6af96792bb5da206518b33d4236fe479155
ehz0ah <130889443+ehz0ah@users.noreply.github.com>
2023-05-01 14:22:29 +0800

    Time: 52 ms (5.78%) | Memory: 16.3 MB (9.61%) - LeetSync

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -6,13 +6,14 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # current node points to the node you're on now. stack is the list of nodes to be visted and res is the value of the nodes that have ben visited and appended into res
         currentNode = root
         stack, res = [],[]
         while stack or currentNode:
             while currentNode is not None:
                 stack.append(currentNode)
                 currentNode = currentNode.left
-                
+            # when stack.pop() it gives the lest most mode hence it will be in order and then append it into res
             currentNode = stack.pop()
             res.append(currentNode.val)
             currentNode = currentNode.right
