bfc7f3736c48db03ee7f39dbc6cbdd4d19e0b646
ehz0ah <130889443+ehz0ah@users.noreply.github.com>
2023-07-12 22:32:03 +0800

    Time: 51 ms (32.04%) | Memory: 16.3 MB (34.67%) - LeetSync

--- pascals-triangle-ii/pascals-triangle-ii.py
+++ pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def getRow(self, n: int) -> List[int]:
-        prev = [1]
-        curr = [1,1]
+        ## prev = [1]
+        ## curr = [1,1]
         if n == 0:
-            return prev
+            return [1]
         if n == 1:
-            return curr
-        for i in range(2,n+1):
-            prev = curr
-            curr = [1]
-            for j in range(1,i):
-                curr.append(prev[j-1] + prev[j])
-            curr.append(1)
+            return [1,1]
+        # for i in range(2,n+1):
+        prev = self.getRow(n-1)
+        curr = [1]
+        for j in range(1,n):
+            curr.append(prev[j-1] + prev[j])
+        curr.append(1)
         return curr
         
